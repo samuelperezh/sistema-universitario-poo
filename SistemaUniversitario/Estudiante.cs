067e8e961e2bbac6917021cb4873a03fa377ee8c
davidromerordz <95779398+davidromerordz@users.noreply.github.com>
2022-03-31 08:35:23 -0500

    Atributos Estudiante

--- SistemaUniversitario/Estudiante.cs
+++ SistemaUniversitario/Estudiante.cs
@@ -6,5 +6,13 @@ namespace SistemaUniversitario
 {
     class Estudiante
     {
+        private string nombre;
+        private string id;
+        private double calificacion_final;
+        private bool es_aprobado;
+        private List<Materia> materias = new List<Materia>();
+        private List<Estudiante> estudiantes = new List<Estudiante>();
+
+
     }
 }
