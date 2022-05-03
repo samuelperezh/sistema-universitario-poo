052ec171dc32e3cc496a55c3f2c074ab593c1a65
samuelph22 <samuelperezh22@gmail.com>
2022-05-02 19:00:54 -0500

    Actualización clase Profesor

--- SistemaUniversitario/Profesor.cs
+++ SistemaUniversitario/Profesor.cs
@@ -7,24 +7,24 @@ namespace SistemaUniversitario
         private string nombre;
         private string id;
 
-        public Profesor(string nombre)
+        public Profesor(string nombre, string id)
         {
             this.Nombre = nombre;
-            this.Id = GenerarID();
+            this.Id = id;
         }
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string Id { get => id; set => id = value; }
 
-        public string GenerarID()
-        {
-            Random rnd = new Random();
-            id = DateTime.Today.ToString("yyyy");
-            for (int i = 0; i < 4; i++)
-            {
-                id = "P" + id + rnd.Next(0, 9).ToString();
-            }
-            return id;
-        }
+        //public string GenerarID()
+        //{
+        //    Random rnd = new Random();
+        //    id = DateTime.Today.ToString("yyyy");
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        id = "P" + id + rnd.Next(0, 9).ToString();
+        //    }
+        //    return id;
+        //}
     }
 }
